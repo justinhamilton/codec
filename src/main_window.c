e1e4d0d608851e9b944827e719c4d0485123ee6a
Justin Hamilton <chezlord45@gmail.com>
2015-04-24 20:55:56 -0400

    Added battery gauge on charge, and some vibrations

--- src/main_window.c
+++ src/main_window.c
@@ -5,19 +5,28 @@ GBitmap *s_res_background_image;
 BitmapLayer *s_bitmaplayer_1;
 Layer *s_layer_1, *background_layer;
 
-Layer *level1layer, *level2layer, *level3layer, *level4layer, *level5layer, *level6layer;
+TextLayer *seconds_layer;
 
 GPath *level1, *level2, *level3, *level4, *level5, *level6;
 
 GFont *metalGear9;
 
-AppTimer *redraw_timer;
-
 TextLayer *time_layer, *bluetooth_layer, *charge_layer;
 
+AppTimer *info_timer;
+
 bool connected;
+bool charging;
+int battery;
 
 int seconds;
+int disp_seconds;
+
+const uint32_t  charge_vibe_segments[] = {200, 200, 400};
+VibePattern charge_vibe = {
+  .durations = charge_vibe_segments,
+  .num_segments = ARRAY_LENGTH(charge_vibe_segments),
+};
 
 GPathInfo LEVEL_1_POINTS = {
   4,
@@ -118,48 +127,87 @@ void background_update(Layer *layer, GContext *ctx) {
 void levels_update(Layer *layer, GContext *ctx) {
   #ifdef PBL_COLOR
     graphics_context_set_fill_color(ctx, GColorElectricBlue);
+    graphics_context_set_antialiased(ctx, true);
   #else
       graphics_context_set_fill_color(ctx, GColorWhite);
   #endif
-  graphics_fill_rect(ctx, GRect(5,130,5,15), 0, GCornersAll);  
-  graphics_fill_rect(ctx, GRect(16,130,24,15), 0, GCornersAll);
-  //gpath_draw_filled(ctx, level1);
-  if(seconds >= 10) {
-     graphics_fill_rect(ctx, GRect(5,110,5,15), 0, GCornersAll);    
-    gpath_draw_filled(ctx, level2);
-  }
- if(seconds >= 20) { 
-    graphics_fill_rect(ctx, GRect(5,90,5,15), 0, GCornersAll);  
-    gpath_draw_filled(ctx, level3);
-  }
-  if(seconds >= 30) {
-    graphics_fill_rect(ctx, GRect(5,70,5,15), 0, GCornersAll);    
-    gpath_draw_filled(ctx, level4);
-  }
-  if(seconds >= 40) {  
-    graphics_fill_rect(ctx, GRect(5,50,5,15), 0, GCornersAll);  
-    gpath_draw_filled(ctx, level5);
-  }
-  if (seconds >= 50) {
-    graphics_fill_rect(ctx, GRect(5,29,5,15), 0, GCornersAll);  
-    graphics_fill_rect(ctx, GRect(16,29,119,15), 0, GCornersAll);
-    #ifdef PBL_COLOR
-      graphics_context_set_text_color(ctx, GColorElectricBlue);
-    #else
-      graphics_context_set_text_color(ctx, GColorWhite);
-    #endif
+  if(charging) {
+    if(battery >= 10) {
+      graphics_fill_rect(ctx, GRect(5,130,5,15), 0, GCornersAll);  
+      graphics_fill_rect(ctx, GRect(16,130,24,15), 0, GCornersAll);
+    }
+    if(battery >= 30) {
+      graphics_fill_rect(ctx, GRect(5,110,5,15), 0, GCornersAll);    
+      gpath_draw_filled(ctx, level2);
+    }
+    if(battery >= 50) {
+      graphics_fill_rect(ctx, GRect(5,90,5,15), 0, GCornersAll);  
+      gpath_draw_filled(ctx, level3);
+    }
+    if(battery >= 70) {
+      graphics_fill_rect(ctx, GRect(5,70,5,15), 0, GCornersAll);    
+      gpath_draw_filled(ctx, level4);
+    }
+    if(battery >= 90) {
+      graphics_fill_rect(ctx, GRect(5,50,5,15), 0, GCornersAll);  
+      gpath_draw_filled(ctx, level5);
+    }
+    if(battery == 100) {
+      graphics_fill_rect(ctx, GRect(5,29,5,15), 0, GCornersAll);  
+      graphics_fill_rect(ctx, GRect(16,29,119,15), 0, GCornersAll);
+      #ifdef PBL_COLOR
+        graphics_context_set_text_color(ctx, GColorElectricBlue);
+      #else
+        graphics_context_set_text_color(ctx, GColorWhite);
+      #endif
+    } else {
+      #ifdef PBL_COLOR
+        graphics_context_set_text_color(ctx, GColorMidnightGreen);
+      #else
+        graphics_context_set_text_color(ctx, GColorBlack);
+      #endif
+    }
   } else {
-    #ifdef PBL_COLOR
-      graphics_context_set_text_color(ctx, GColorMidnightGreen);
-    #else
-      graphics_context_set_text_color(ctx, GColorBlack);
-    #endif
+    graphics_fill_rect(ctx, GRect(5,130,5,15), 0, GCornersAll);  
+    graphics_fill_rect(ctx, GRect(16,130,24,15), 0, GCornersAll);
+    if(seconds >= 10) {
+       graphics_fill_rect(ctx, GRect(5,110,5,15), 0, GCornersAll);    
+      gpath_draw_filled(ctx, level2);
+    }
+   if(seconds >= 20) { 
+      graphics_fill_rect(ctx, GRect(5,90,5,15), 0, GCornersAll);  
+      gpath_draw_filled(ctx, level3);
+    }
+    if(seconds >= 30) {
+      graphics_fill_rect(ctx, GRect(5,70,5,15), 0, GCornersAll);    
+      gpath_draw_filled(ctx, level4);
+    }
+    if(seconds >= 40) {  
+      graphics_fill_rect(ctx, GRect(5,50,5,15), 0, GCornersAll);  
+      gpath_draw_filled(ctx, level5);
+    }
+    if (seconds >= 50) {
+      graphics_fill_rect(ctx, GRect(5,29,5,15), 0, GCornersAll);  
+      graphics_fill_rect(ctx, GRect(16,29,119,15), 0, GCornersAll);
+      #ifdef PBL_COLOR
+        graphics_context_set_text_color(ctx, GColorElectricBlue);
+      #else
+        graphics_context_set_text_color(ctx, GColorWhite);
+      #endif
+    } else {
+      #ifdef PBL_COLOR
+        graphics_context_set_text_color(ctx, GColorMidnightGreen);
+      #else
+        graphics_context_set_text_color(ctx, GColorBlack);
+      #endif
+    }
   }
    graphics_draw_text(ctx, "MaX",  metalGear9, GRect(114,43,30,30), GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);
 }
 
 void set_time(struct tm *t) {
  static char time_text[] = "00.00";
+  static char seconds_text[] = "000%";
   seconds = t->tm_sec;
   if(seconds == 10 || seconds == 20 || seconds == 30 || seconds == 40 || seconds == 50 || seconds == 60) {
       layer_mark_dirty(s_layer_1);
@@ -169,7 +217,18 @@ void set_time(struct tm *t) {
   } else {
     strftime(time_text, sizeof(time_text), "%I.%M", t);
   }
+  disp_seconds = seconds;
+  while(disp_seconds >= 0) {
+    disp_seconds -= 10;
+  }
+  disp_seconds += 10;
+  if(charging) {
+    snprintf(seconds_text, sizeof(seconds_text), "%i", battery);
+  } else {
+    snprintf(seconds_text, sizeof(seconds_text), "%i", disp_seconds);
+  }
   text_layer_set_text(time_layer, time_text);
+  text_layer_set_text(seconds_layer, seconds_text);
 }
 
 void tick_handler(struct tm *t, TimeUnits units_changed) {
@@ -177,10 +236,32 @@ void tick_handler(struct tm *t, TimeUnits units_changed) {
 }
 
 void handle_battery(BatteryChargeState charge_state) {
-  if(charge_state.charge_percent <= 20) {
-    layer_set_hidden((Layer*)charge_layer, false);
+  if(charge_state.is_plugged) {
+    battery = charge_state.charge_percent;
+    charging = true;
+    #ifdef PBL_COLOR
+      layer_set_hidden((Layer*)charge_layer, false);
+      if(charge_state.charge_percent == 100) {
+         text_layer_set_background_color(charge_layer, GColorGreen);
+      } else {
+         text_layer_set_background_color(charge_layer, GColorChromeYellow);
+      }
+    #else
+      layer_set_hidden((Layer*)charge_layer, false);
+    #endif
   } else {
-     layer_set_hidden((Layer*)charge_layer, true);
+    charging = false;
+    #ifdef PBL_COLOR
+      text_layer_set_background_color(charge_layer, GColorRed);
+    #endif
+    if(charge_state.charge_percent <= 20) {
+      if(charge_state.charge_percent == 20) {
+      vibes_enqueue_custom_pattern(charge_vibe);
+      }
+      layer_set_hidden((Layer*)charge_layer, false);
+    } else {
+      layer_set_hidden((Layer*)charge_layer, true);
+    }
   }
 }
 
@@ -192,6 +273,7 @@ void handle_bluetooth(bool connection) {
       text_layer_set_text_color(bluetooth_layer, GColorWhite);
     #endif
   } else {
+    vibes_short_pulse();
     #ifdef PBL_COLOR
       text_layer_set_text_color(bluetooth_layer, GColorMidnightGreen);
     #else
@@ -234,7 +316,7 @@ void initialise_ui() {
   text_layer_set_font(time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_METAL_GEAR_17)));
   layer_add_child(window_get_root_layer(s_window), (Layer*)time_layer);
   
-  charge_layer = text_layer_create(GRect(52,115,88,21));
+  charge_layer = text_layer_create(GRect(52,112,88,21));
   text_layer_set_text(charge_layer, "CHRG");
   #ifdef PBL_COLOR
     text_layer_set_background_color(charge_layer, GColorRed);
@@ -259,6 +341,17 @@ void initialise_ui() {
   text_layer_set_font(bluetooth_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_METAL_GEAR_13)));
   layer_add_child(window_get_root_layer(s_window), (Layer*)bluetooth_layer);
   
+  seconds_layer = text_layer_create(GRect(44, 136, 120, 30));
+  text_layer_set_text(seconds_layer, "0");
+  text_layer_set_font(seconds_layer, metalGear9);
+  #ifdef PBL_COLOR
+    text_layer_set_text_color(seconds_layer, GColorElectricBlue);
+  #else
+    text_layer_set_text_color(seconds_layer, GColorWhite);
+  #endif
+  text_layer_set_background_color(seconds_layer, GColorClear);
+  layer_add_child(window_get_root_layer(s_window), (Layer*)seconds_layer);
+    
   level1 = gpath_create(&LEVEL_1_POINTS);
   level2 = gpath_create(&LEVEL_2_POINTS);
   level3 = gpath_create(&LEVEL_3_POINTS);
@@ -268,6 +361,12 @@ void initialise_ui() {
   
   handle_battery(battery_state_service_peek());
   handle_bluetooth(bluetooth_connection_service_peek());
+  
+  
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  time_t temp = time(NULL);
+  struct tm *t = localtime(&temp);
+  set_time(t);
 }
 
 void destroy_ui() {
@@ -293,11 +392,6 @@ void show_main_window() {
   bluetooth_connection_service_subscribe(&handle_bluetooth);
   battery_state_service_subscribe(&handle_battery);
   window_stack_push(s_window, true);
-  
-  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
-  time_t temp = time(NULL);
-  struct tm *t = localtime(&temp);
-  set_time(t);
 }
 
 void hide_main_window() {
